c2fec01993693ae0a1da8b75814703dced04db3a
Raph <raphaelpaquin19@gmail.com>
2024-08-26 10:49:38 -0400

    Init Create

---
+++ src/test/java/com/cinq/labo_1/Labo1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cinq.labo_1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Labo1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
